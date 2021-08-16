d9b08a24b48b3ef10f977ec5d4429e411ecbc512
theludovyc <theludovyc@gmail.com>
2021-08-16 23:35:37 +0200

    start in GDialog instead of GDialog_Class

--- addons/GDialog/Other/GDialog.gd
+++ addons/GDialog/Other/GDialog.gd
@@ -52,8 +52,6 @@ enum Event_Type{
 ## *****************************************************************************
 ##								INITIALIZATION
 ## *****************************************************************************
-
-
 func _init() -> void:
 	var directory = Directory.new()
 	
@@ -187,11 +185,82 @@ func set_glossary_from_id(id: String, title: String, text: String, extra:String)
 ##								TIMELINES
 ## *****************************************************************************
 
+## Starts the dialog for the given timeline and returns a Dialog node.
+## You must then add it manually to the scene to display the dialog.
+##
+## Example:
+## var new_dialog = GDialog.start("Your Timeline Name Here")
+## add_child(new_dialog)
+##
+## This is exactly the same as using the editor:
+## you can drag and drop the scene located at /addons/GDialog/Dialog.tscn 
+## and set the current timeline via the inspector.
+##
+## @param timeline				The timeline to load. You can provide the timeline name or the filename.
+## @param reset_saves			True to reset GDialog saved data such as definitions.
+## @param dialog_scene_path		If you made a custom Dialog scene or moved it from its default path, you can specify its new path here.
+## @param debug_mode			Debug is disabled by default but can be enabled if needed.
+## @param use_canvas_instead	Create the Dialog inside a canvas layer to make it show up regardless of the camera 2D/3D situation.
+## @returns						A Dialog node to be added into the scene tree.
+func start(timeline_name: String, reset_saves: bool=true, dialog_scene_path: String="res://addons/GDialog/Dialog.tscn", debug_mode: bool=false, use_canvas_instead=true):
+	var dialog_scene = load(dialog_scene_path)
+	var dialog_node = null
+	var canvas_dialog_node = null
+	var returned_dialog_node = null
+	
+	if use_canvas_instead:
+		var canvas_dialog_script = load("res://addons/GDialog/Nodes/canvas_dialog_node.gd")
+		canvas_dialog_node = canvas_dialog_script.new()
+		canvas_dialog_node.set_dialog_node_scene(dialog_scene)
+		dialog_node = canvas_dialog_node.dialog_node
+	else:
+		dialog_node = dialog_scene.instance()
+	
+	dialog_node.reset_saves = reset_saves
+	dialog_node.debug_mode = debug_mode
+	
+	returned_dialog_node = dialog_node if not canvas_dialog_node else canvas_dialog_node
+	
+	if !timeline_name.empty() and timelines.has(timeline_name):
+		dialog_node.timeline_name = timeline_name
+		
+		return returned_dialog_node
+	
+	dialog_node.dialog_script = {
+		"events":[
+			{"type": Event_Type.Text,
+			"character":"",
+			"portrait":"",
+			"text":"[Dialogic Error] Loading dialog [color=red]" + timeline_name + "[/color]. It seems like the timeline doesn't exists. Maybe the name is wrong?"
+		}]}
+		
+	return returned_dialog_node
+
+## Same as the start method above, but using the last timeline saved.
+## 
+## @param initial_timeline		The timeline to load in case no save is found.
+## @param dialog_scene_path		If you made a custom Dialog scene or moved it from its default path, you can specify its new path here.
+## @param debug_mode			Debug is disabled by default but can be enabled if needed.
+## @returns						A Dialog node to be added into the scene tree.
+func start_from_save(initial_timeline: String, dialog_scene_path: String="res://addons/GDialog/Dialog.tscn", debug_mode: bool=false):
+	var current := get_current_timeline()
+	if current.empty():
+		current = initial_timeline
+	return start(current, false, dialog_scene_path, debug_mode)
+
+## Sets the currently saved timeline.
+## Use this if you disabled current timeline autosave and want to control it yourself
+##
+## @param timelinie						The new timeline to save.
 func set_current_timeline(timeline: String):
 	current_timeline = timeline
 	set_saved_state_general_key('timeline', timeline)
 
-
+## Gets the currently saved timeline.
+## Timeline saves are set on timeline start, and cleared on end.
+## This means you can keep track of timeline changes and detect when the dialog ends.
+##
+## @returns						The current timeline filename, or an empty string if none was saved.
 func get_current_timeline() -> String:
 	return current_timeline
 
--- addons/GDialog/Other/GDialog_Class.gd
+++ addons/GDialog/Other/GDialog_Class.gd
@@ -11,73 +11,6 @@ extends Node
 ## Trying to follow this documentation convention: https://github.com/godotengine/godot/pull/41095
 class_name GDialog_Class
 
-
-## Refactor the start function for 2.0 there should be a cleaner way to do it :)
-
-## Starts the dialog for the given timeline and returns a Dialog node.
-## You must then add it manually to the scene to display the dialog.
-##
-## Example:
-## var new_dialog = Dialogic.start('Your Timeline Name Here')
-## add_child(new_dialog)
-##
-## This is exactly the same as using the editor:
-## you can drag and drop the scene located at /addons/GDialog/Dialog.tscn 
-## and set the current timeline via the inspector.
-##
-## @param timeline				The timeline to load. You can provide the timeline name or the filename.
-## @param reset_saves			True to reset GDialog saved data such as definitions.
-## @param dialog_scene_path		If you made a custom Dialog scene or moved it from its default path, you can specify its new path here.
-## @param debug_mode			Debug is disabled by default but can be enabled if needed.
-## @param use_canvas_instead	Create the Dialog inside a canvas layer to make it show up regardless of the camera 2D/3D situation.
-## @returns						A Dialog node to be added into the scene tree.
-static func start(timeline_name: String, reset_saves: bool=true, dialog_scene_path: String="res://addons/GDialog/Dialog.tscn", debug_mode: bool=false, use_canvas_instead=true):
-	var dialog_scene = load(dialog_scene_path)
-	var dialog_node = null
-	var canvas_dialog_node = null
-	var returned_dialog_node = null
-	
-	if use_canvas_instead:
-		var canvas_dialog_script = load("res://addons/GDialog/Nodes/canvas_dialog_node.gd")
-		canvas_dialog_node = canvas_dialog_script.new()
-		canvas_dialog_node.set_dialog_node_scene(dialog_scene)
-		dialog_node = canvas_dialog_node.dialog_node
-	else:
-		dialog_node = dialog_scene.instance()
-	
-	dialog_node.reset_saves = reset_saves
-	dialog_node.debug_mode = debug_mode
-	
-	returned_dialog_node = dialog_node if not canvas_dialog_node else canvas_dialog_node
-	
-	if !timeline_name.empty() and GDialog.timelines.has(timeline_name):
-		dialog_node.timeline_name = timeline_name
-		
-		return returned_dialog_node
-	
-	dialog_node.dialog_script = {
-		"events":[
-			{"type": GDialog.Event_Type.Text,
-			"character":"",
-			"portrait":"",
-			"text":"[Dialogic Error] Loading dialog [color=red]" + timeline_name + "[/color]. It seems like the timeline doesn't exists. Maybe the name is wrong?"
-		}]}
-		
-	return returned_dialog_node
-
-
-## Same as the start method above, but using the last timeline saved.
-## 
-## @param initial_timeline		The timeline to load in case no save is found.
-## @param dialog_scene_path		If you made a custom Dialog scene or moved it from its default path, you can specify its new path here.
-## @param debug_mode			Debug is disabled by default but can be enabled if needed.
-## @returns						A Dialog node to be added into the scene tree.
-static func start_from_save(initial_timeline: String, dialog_scene_path: String="res://addons/GDialog/Dialog.tscn", debug_mode: bool=false):
-	var current := get_current_timeline()
-	if current.empty():
-		current = initial_timeline
-	return start(current, false, dialog_scene_path, debug_mode)
-
 ## Gets default values for definitions.
 ## 
 ## @returns						Dictionary in the format {'variables': [], 'glossary': []}
@@ -138,24 +71,6 @@ static func get_glossary(name: String) -> Dictionary:
 ## @param extra					The extra information at the bottom of the box.
 static func set_glossary(name: String, title: String, text: String, extra: String) -> void:
 	GDialog.set_glossary(name, title, text, extra)
-
-
-## Gets the currently saved timeline.
-## Timeline saves are set on timeline start, and cleared on end.
-## This means you can keep track of timeline changes and detect when the dialog ends.
-##
-## @returns						The current timeline filename, or an empty string if none was saved.
-static func get_current_timeline() -> String:
-	return GDialog.get_current_timeline()
-
-
-## Sets the currently saved timeline.
-## Use this if you disabled current timeline autosave and want to control it yourself
-##
-## @param timelinie						The new timeline to save.
-static func set_current_timeline(new_timeline: String) -> String:
-		return GDialog.set_current_timeline(new_timeline)
-
 
 ## Export the current Dialogic state.
 ## This can be used as part of your own saving mechanism if you have one. If you use this,
