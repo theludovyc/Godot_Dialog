e7ae23af5f7f1d8dd5192c1a227ab622aca34821
theludovyc <theludovyc@gmail.com>
2021-08-18 12:27:22 +0200

    event set mood works

--- addons/GDialog/Editor/Events/Parts/ResourcePickers/Characters/EventPart_PortraitPicker.gd
+++ addons/GDialog/Editor/Events/Parts/ResourcePickers/Characters/EventPart_PortraitPicker.gd
@@ -1,18 +1,19 @@
 tool
 extends EventPart
 
-# has an event_data variable that stores the current data!!!
-
-export (bool) var allow_dont_change := true
-export (bool) var allow_definition := true
-
 ## node references
 onready var picker_menu = $HBox/MenuButton
 
+var popup_menu
+
 # used to connect the signals
 func _ready():
-	picker_menu.get_popup().connect("index_pressed", self, '_on_PickerMenu_selected')
 	picker_menu.connect("about_to_show", self, "_on_PickerMenu_about_to_show")
+	
+	popup_menu = picker_menu.get_popup()
+	
+	popup_menu.connect("index_pressed", self, '_on_PickerMenu_selected')
+	
 
 # called by the event block
 func load_data(data:Dictionary):
@@ -28,35 +29,21 @@ func get_preview():
 	return ''
 
 func _on_PickerMenu_selected(index):
-	if index == 0 and allow_dont_change:
-		event_data['portrait'] = "(Don't change)"
-	elif allow_definition and ((allow_dont_change and index == 1) or index == 0):
-		event_data['portrait'] = "[Definition]"
-	else:
-		event_data['portrait'] = picker_menu.get_popup().get_item_text(index)
+	var name = popup_menu.get_item_text(index)
 	
-	picker_menu.text = event_data['portrait']
+	event_data["portrait"] = name
+	
+	picker_menu.text = name
 	
 	# informs the parent about the changes!
 	data_changed()
 
-func get_character_data():
-	for ch in GDialog_Util.get_character_list():
-		if ch['file'] == event_data['character']:
-			return ch
-
 func _on_PickerMenu_about_to_show():
-	picker_menu.get_popup().clear()
-	var index = 0
-	if allow_dont_change:
-		picker_menu.get_popup().add_item("(Don't change)")
-		index += 1
-	if allow_definition:
-		picker_menu.get_popup().add_item("[Definition]")
-		index += 1
-	if event_data['character']:
-		var character = get_character_data()
-		if character.has('portraits'):
-			for p in character['portraits']:
-				picker_menu.get_popup().add_item(p['name'])
-				index += 1
+	popup_menu.clear()
+	
+	if event_data.has("character"):
+		var character = editor_reference.characters[event_data["character"]]
+		
+		if character.has("portraits"):
+			for p in character["portraits"]:
+				popup_menu.add_item(p["name"])
