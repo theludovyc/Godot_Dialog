bbb7ab2ae1e7f99c1710b96479a229a006741bbe
theludovyc <theludovyc@gmail.com>
2021-09-11 12:11:26 +0200

    remake setValue event

--- addons/GDialog/Editor/Events/Parts/Logic/EventPart_OperationPicker.gd
+++ addons/GDialog/Editor/Events/Parts/Logic/EventPart_OperationPicker.gd
@@ -19,22 +19,16 @@ func on_ready():
 	for value in operations.values():
 		picker_popup.add_item(value)
 	
-	printt("OperationPicker", "hello")
-	
 	picker_popup.connect("index_pressed", self, '_on_PickerMenu_selected')
 
 # called by the event block
-func load_data(data:Dictionary):
-	# First set the event_data
-	.load_data(data)
-	
-	# Now update the ui nodes to display the data.
+func init_data(data:Dictionary):
 	if !data.has("operation"):
-		event_data["operation"] = "="
+		data["operation"] = "="
 		
-		data_changed()
+		send_data({"operation":"="})
 	
-	picker_menu.text = operations[event_data["operation"]]
+	picker_menu.text = operations[data["operation"]]
 	
 # has to return the wanted preview, only useful for body parts
 func get_preview():
@@ -43,9 +37,7 @@ func get_preview():
 func _on_PickerMenu_selected(index):
 	var text = picker_popup.get_item_text(index)
 	
-	event_data["operation"] = operations.keys()[index]
-	
 	picker_menu.text = text
 	
 	# informs the parent about the changes!
-	data_changed()
+	send_data({"operation":operations.keys()[index]})
